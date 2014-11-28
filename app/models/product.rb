14d86b305e3151f7b87e3e163102a2b7312eeb1d
Rolando Estrella <rolando.estrella25@gmail.com>
2014-11-27 21:08:37 -0600

    1.2 added 20 products and validations for product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,24 @@ class Product < ActiveRecord::Base
 
   validates :condition, inclusion: {
     in: %w(mint played),
-    message: "Condition must be 'mint' or 'played' not %{value}"
+    message: "must be 'mint' or 'played' not %{value}"
+  }
+
+  validates :price, :stock_quantity, numericality: {
+    greater_than_or_equal_to: 0
+  }
+
+  validates :price, format: {
+    with: /\A\d+(?:\.\d{0,2})?\z/,
+    message: "must be 0..2 decimal places."
+  }
+
+  validates :stock_quantity, numericality: {
+    only_integer: true
+  }
+
+  validates :image_filename, allow_blank: true, format: {
+    with: %r{\.gif|jpg|png}i,
+    message: "must end with .gif, .jpg, or .png"
   }
 end
